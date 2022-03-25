6f159ebd0e88cea86f7a67847449fe15d17c386b
TmasNoemi <tomasellonoemi@gmail.com>
2022-03-25 20:47:58 +0100

    Update pipeline.py
    Working pipeline

--- bertserini/pipeline.py
+++ bertserini/pipeline.py
@@ -1,9 +1,13 @@
 from reader import BERT
+import argparse
+from utils.structures import Question, Answer
+from pyserini.search.lucene import  LuceneSearcher
+from retriever import retrieve
 
 class BERTserini:
     def __init__(self, question_string, model_to_load):
         self.question = Question(question_string)
-        self.searcher = LuceneSearcher.from_prebuilt_index("enwiki-paragraphs")
+        self.searcher = LuceneSearcher.from_prebuilt_index("wikipedia-dpr")
         self.bert = BERT(model_to_load)
 
     def retrieve(self):
@@ -14,8 +18,10 @@ class BERTserini:
 
     def answer(self):
         possible_questions = self.bert.predict(self.question, self.contexts)
-        answer = get_best_answer(possible_questions, 0.45)
-        return answer.text
+        for ans in possible_questions:
+            ans.aggregate_score(0.80)
+        best = sorted(possible_questions, key=lambda x: x.total_score, reverse=True)[0]
+        return best.text
     
     def get_question(self):
         return self.question.text
@@ -23,13 +29,22 @@ class BERTserini:
  
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, help="model to load", required=True)
-    parser.add_argument('question_text', type=str, required=True, help="Question to ask the model")
+    parser.add_argument('--model', type=str, help="model to load")
+    parser.add_argument('--question_text', type=str, help="Question to ask the model")
 
 
     args = parser.parse_args()
 
     bertserini = BERTserini(args.question_text ,args.model)
+    print("*"*60)
+    print("Retrieving contexts...")
+    print("*"*60)
     bertserini.retrieve()
+    print("*"*60)
+    print("Finding answering...")
+    print("*"*60)
     risposta = bertserini.answer()
+    print("*"*60)
+    print("Answer:")
     print(risposta)
+    print("*"*60)
